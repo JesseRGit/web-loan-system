6b0c9456df360088823622e213518efdce4c25ac
Jesse Rönkkö <jesse.ronkko@eng.tamk.fi>
2018-09-08 15:52:42 +0300

    Task 1 b done

--- with-typescript-app/src/Home.tsx
+++ with-typescript-app/src/Home.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import React from 'react';
 import React1a from './components/React1a'
+import React1b from './components/React1b'
 
 const Home: React.SFC<{prop1: string, prop2: number}> = () => {
 return (
@@ -10,6 +11,7 @@ return (
       <a>About</a>
     </Link>
     <React1a />
+    <React1b />
   </div>
 )
 }
