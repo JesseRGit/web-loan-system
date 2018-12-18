08d9504b7d6072f4819a0a802b0cc097c03ffad4
Jesse Rönkkö <jesse.ronkko@eng.tamk.fi>
2018-12-18 16:37:41 +0200

    Shiwing local equipments works

--- src/components/ShowEquipments.tsx
+++ src/components/ShowEquipments.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-const ShowEquipments: React.SFC<{prop1: string, prop2: number}> = () => {
-  return (
-    <div>
-      <h1>ShowEquipments.tsx</h1>
-  </div>
-  )
+//import countries from '../countries';
+//import Country from './Country';
+
+import equipments from '../TestData';
+import Equipment from './Equipment';
+
+class ShowEquipments extends React.Component<{}, IEquipmentsState> {
+  public render() {
+    return (
+        <Paper>
+          <h1>Equipments</h1>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  Id
+                </TableCell>
+                <TableCell>
+                  Name
+                </TableCell>
+                <TableCell>
+                  Description
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {equipments.map(equipment => <Equipment prop1={equipment} />)}
+            </TableBody>
+          </Table>
+        </Paper>
+    )
+  };
 }
 
 export default ShowEquipments;
