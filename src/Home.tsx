77c0b0c1b0cb61d9b33a5b86fa913922be4de8fb
Jesse Rönkkö <jesse.ronkko@eng.tamk.fi>
2018-12-18 18:26:14 +0200

    Showing local equipments works and remotehost ip fixed

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import ButtonAppBar from './components/ButtonAppBar'
 
+//Jäin 2.1 Haku & järjestely (2p)
+
 const Home: React.SFC<{prop1: string, prop2: number}> = () => {
   return (
     <div>
