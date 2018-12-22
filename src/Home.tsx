b8f04fa6d0d6078483c1addc593bb59b18f851d3
Jesse Rönkkö <jesse.ronkko@eng.tamk.fi>
2018-12-22 22:38:37 +0200

    Saving

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,28 +3,15 @@ import ReactDOM from 'react-dom';
 
 import ButtonAppBar from './components/ButtonAppBar'
 
-//Jäin 2.1 Haku & järjestely (2p)
-
-//Korjaa
-/*
-ERROR  Failed to compile with 1 errors
-
-error  in ./node_modules/react-table/react-table.css
-
-Module parse failed: Unexpected token (1:0)
-You may need an appropriate loader to handle this file type.
-
-*/
-
 const Home: React.SFC<{prop1: string, prop2: number}> = () => {
   return (
     <div>
       <div>
         <ButtonAppBar />
       </div>
-      <h1>Hello!</h1>
+      <h1>Home</h1>
       <br />
-      <div>Welcome!</div>
+      <div>Hello this is Home.tsx</div>
       <br />
   </div>
   )
--- src/components/ShowEquipments.tsx
+++ src/components/ShowEquipments.tsx
@@ -1,111 +1,49 @@
-import React from "react";
-import { render } from "react-dom";
-//import { makeData, Logo, Tips } from "./Utils";
-import matchSorter from 'match-sorter'
-
-// Import React Table
+import React, { Component } from 'react';
 import ReactTable from "react-table";
-//import "react-table/react-table.css"; //breaks all
-//import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import "react-table/react-table.css";
 
-//import Paper from '@material-ui/core/Paper';
+class ShowEquipments extends Component {
 
+  render() {
+    const data = [{
+      name: 'Roy Agasthyan',
+      age: 26
+    },{
+      name: 'Sam Thomason',
+      age: 22
+    },{
+      name: 'Michael Jackson',
+      age: 36
+    },{
+      name: 'Samuel Roy',
+      age: 56
+    },{
+      name: 'Rima Soy',
+      age: 28
+    },{
+      name: 'Suzi Eliamma',
+      age: 28
+    }]
 
-const data = [
-  {
-    firstName: 'Jack',
-    lastName: 'Jackson',
-    age: 6,
-    visits: 24,
-    progress: 4,
-    status: "single"
-  },
-  // ...
-  {
-    firstName: 'Boy',
-    lastName: 'Boyson',
-    age: 8,
-    visits: 26,
-    progress: 5,
-    status: "single"
-  }
-]
+    const columns = [{
+      Header: 'Name',
+      accessor: 'name'
+    },{
+      Header: 'Age',
+      accessor: 'age'
+    }]
 
-interface IEquipmentsState {
-  data: any;
-}
-
-class ShowEquipments extends React.Component<{}, IEquipmentsState>  {
-  public render() {
     return (
-      <div>
-        <ReactTable
-          data={data}
-          filterable
-          defaultFilterMethod={(filter, row) =>
-            String(row[filter.id]) === filter.value}
-          columns={[
-            {
-              Header: "Name",
-              columns: [
-                {
-                  Header: "First Name",
-                  accessor: "firstName",
-                  filterMethod: (filter, row) =>
-                    row[filter.id].startsWith(filter.value) &&
-                    row[filter.id].endsWith(filter.value)
-                },
-                {
-                  Header: "Last Name",
-                  id: "lastName",
-                  accessor: d => d.lastName,
-                  filterMethod: (filter, rows) =>
-                    matchSorter(rows, filter.value, { keys: ["lastName"] }),
-                  filterAll: true
-                }
-              ]
-            },
-            {
-              Header: "Info",
-              columns: [
-                {
-                  Header: "Age",
-                  accessor: "age"
-                },
-                {
-                  Header: "Over 21",
-                  accessor: "age",
-                  id: "over",
-                  Cell: ({ value }) => (value >= 21 ? "Yes" : "No"),
-                  filterMethod: (filter, row) => {
-                    if (filter.value === "all") {
-                      return true;
-                    }
-                    if (filter.value === "true") {
-                      return row[filter.id] >= 21;
-                    }
-                    return row[filter.id] < 21;
-                  },
-                  Filter: ({ filter, onChange }) =>
-                    <select
-                      onChange={event => onChange(event.target.value)}
-                      style={{ width: "100%" }}
-                      value={filter ? filter.value : "all"}
-                    >
-                      <option value="all">Show All</option>
-                      <option value="true">Can Drink</option>
-                      <option value="false">Can't Drink</option>
-                    </select>
-                }
-              ]
-            }
-          ]}
-          defaultPageSize={10}
-          className="-striped -highlight"
-        />
-        <br />
-      </div>
-    );
+          <div>
+              <ReactTable
+                data={data}
+                columns={columns}
+                defaultPageSize = {3}
+                pageSizeOptions = {[3, 6]}
+              />
+          </div>
+    )
+
   }
 }
 
