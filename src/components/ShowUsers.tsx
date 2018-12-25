f7136804967c286b33aaec4c1112ce8baa25cbc5
JesseRGit <jesseronkko@hotmail.com>
2018-12-25 22:41:23 +0200

    Deleting local user works

--- src/components/ShowUsers.tsx
+++ src/components/ShowUsers.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactTable from 'react-table';
 import users from '../users';
-
-const columns = [
-{ Header: "Id", accessor: "id", style:{ textAlign: "Right" }, width:100, minWidth: 100, maxWidth: 100 },
-{ Header: "Name", accessor: "name" },
-{ Header: "Email", accessor: "email" },
-{ Header: "Actions", filterable: false, Cell: props =>{ return(<button className="">Delete</button>)}}]
+import Icon from '@material-ui/core/Icon';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 interface IUsersState {
   data: any;
@@ -16,10 +12,36 @@ class ShowUsers extends React.Component <{}, IUsersState> {
   constructor(props) {
     super(props)
     this.state = {
-      data: ['testitem'],
+      users: ['testitem'],
     }
   }
+
+  deleteRow(id){
+    //console.log("id", id)
+    const index = users.findIndex(user =>{
+      return user.id === id
+    })
+    users.splice(index, 1)
+    this.setState(users)
+  }
+
    public render() {
+     const columns = [
+     { Header: "Id", accessor: "id", style:{ textAlign: "Right" }, width:100, minWidth: 100, maxWidth: 100 },
+     { Header: "Name", accessor: "name" },
+     { Header: "Email", accessor: "email" },
+     { Header: "Actions", filterable: false,
+       style:{ textAlign: "Right" }, width:100,
+       minWidth: 100, maxWidth: 100, Cell: props =>{
+         return(
+           <button onClick={() =>{
+             //console.log("props", props)
+             this.deleteRow(props.original.id);
+           }}
+           >
+            <Icon><DeleteIcon /></Icon>
+           </button>
+         )}}]
     return (
       <div>
         <ReactTable
