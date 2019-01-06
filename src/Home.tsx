af2852fecd5d5902339837a21bfcb1421f0747da
JR <jesse.ronkko@eng.tamk.fi>
2019-01-06 11:46:57 +0200

    Saving before merging to master

--- src/Home.tsx
+++ src/Home.tsx
@@ -10,7 +10,7 @@ const Home: React.SFC<{prop1: string, prop2: number}> = () => {
         <TopNav />
       </div>
       <h1>Welcome!</h1>
-      <div>You can use this manage loan system's devices, users and loans.</div>
+      <div>You can use this add, edit and remove loan system's devices, users and loans.</div>
       <div>Start by clicking top left icon and selecting what you want to see.</div>
   </div>
   )
